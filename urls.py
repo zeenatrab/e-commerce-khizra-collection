6f64c0d17171577e895f497b5ca3993eb1bd5cd6
zeenatrab <60963286+zeenatrab@users.noreply.github.com>
2022-01-14 20:58:54 +0530

    Add files via upload
    kzc / templets py

--- urls.py
+++ urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopHome"),
-    path("about/", views.about, name="AboutUs"),
-    path("contact/", views.contact, name="ContactUs"),
-    path("tracker/", views.tracker, name="TrackingStatus"),
-    path("search/", views.search, name="Search"),
-    path("products/<int:myid>", views.productView, name="ProductView"),
-    path("checkout/", views.checkout, name="Checkout"),
-    #path("handlerequest/", views.handlerequest, name="HandleRequest"),
-
-]
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls')),
+    path('', views.index)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
